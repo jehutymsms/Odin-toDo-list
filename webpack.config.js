468e60a0ae6327f29df4095a7be1c2e24236baf8
Andrew Humphries <andrewhumphrieswork@outlook.com>
2021-11-29 19:35:12 -0600

    Updated for live-reload

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development',
       template: 'src/index.html',
-
     }),
   ],
   output: {
