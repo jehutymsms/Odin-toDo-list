e52ca18a4828059faddd0540e09dd2db028f9972
Andrew Humphries <andrewhumphrieswork@outlook.com>
2022-01-05 07:42:11 -0600

    Element Creation Start

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@
 import './style.css';
 import { userStorage } from './userStorage';
 import { signInSection } from './signInSectionFiles/signInSection';
+import { signInSectionCreation } from './signInSectionFiles/signInSectionCreation';
 // console.log('Now time for the JS')
+
+signInSectionCreation.formElementContentList()
--- src/signInSectionFiles/signInSectionCreation.js
+++ src/signInSectionFiles/signInSectionCreation.js
@@ -2,7 +2,7 @@ import { globaljs } from '../global';
 
 export const signInSectionCreation = (() => {
     const cacheDom = (() => {
-        let body = document.getElementsByTagName('body')
+        let body = document.getElementsByTagName('body');
         return { body: body };
     })();
 
@@ -20,15 +20,69 @@ export const signInSectionCreation = (() => {
 
         signInHTMLs : ['Welcome to Checkist', 'Username','Password','Demo'],
 
-      }
+      };
     
       //Fuction List
 
       //Return Form Element Contents as list
-      const formElementContentList = (objectList) => {
-        for(let i = 0; i<5;i++){
-            console.log(i+1)
-        }
+      const formElementContentList = () => {
+        let contentList = [];
+
+        // for (let i = 0; i < 2; i++) {
+        //     let label[`${x}`] = globaljs.newElement({
+        //         tag:SignInObjectList.signInTags[3],
+        //         for:SignInObjectList.signInFors[0],
+        //         htmlString:SignInObjectList.signInHTMLs[1],
+        //     })
+        // }
+
+        let label1 = globaljs.newElement({
+            tag:SignInObjectList.signInTags[3],
+            for:SignInObjectList.signInFors[0],
+            htmlString:SignInObjectList.signInHTMLs[1],
+        })
+
+        let label2 = globaljs.newElement({
+            tag:SignInObjectList.signInTags[3],
+            for:SignInObjectList.signInFors[1],
+            htmlString:SignInObjectList.signInHTMLs[2],
+        })
+
+        let input1 = globaljs.newElement({
+            tag:SignInObjectList.signInTags[4],
+            type:SignInObjectList.signInTypes[0],
+            id:SignInObjectList.signInIds[5],
+            name:SignInObjectList.signInIds[5],
+        })
+
+        let input2 = globaljs.newElement({
+            tag:SignInObjectList.signInTags[4],
+            type:SignInObjectList.signInTypes[0],
+            id:SignInObjectList.signInIds[6],
+            name:SignInObjectList.signInIds[6],
+        })
+
+        let input3 = globaljs.newElement({
+            tag:SignInObjectList.signInTags[4],
+            type:SignInObjectList.signInTypes[1],
+            value:SignInObjectList.signInValues[0],
+        })
+
+        let input4 = globaljs.newElement({
+            tag:SignInObjectList.signInTags[4],
+            type:SignInObjectList.signInTypes[1],
+            value:SignInObjectList.signInValues[1],
+        })
+
+
+        contentList.push(label1)
+        contentList.push(input1)
+        contentList.push(label2)
+        contentList.push(input2)
+        contentList.push(input3)
+        contentList.push(input4)
+
+        return contentList
       } 
       
       //Returns Form Element Section
@@ -49,5 +103,5 @@ export const signInSectionCreation = (() => {
         return {bodyAppendChild: bodyAppendChild };
     })();
 
-    return{}
-})()
+    return{formElementContentList:formElementContentList,render:render};
+})();
